89be7af4ad51d807b7a06ebaad17f8c3dc50962f
JC <jendycaprico@gmail.com>
2025-11-11 22:38:57 +0700

    fix/chroma-langchain
    - fix(env): adjust env to match langchain chroma docs
    - fix(agent): adjust chroma langchain initiation to handle chroma cloud case
    - fix(chroma): adjust env for chroma module
    - fix(eval): remove test endpoint in chroma controller
    - chore(env): adjust env example

--- src/modules/agent/agent.module.ts
+++ src/modules/agent/agent.module.ts
@@ -3,68 +3,67 @@ import { RagAgent } from './rag/rag.agent';
 import { MongodbModule } from '../database/mongodb/mongodb.module';
 import { ChromaDbModule } from '../database/chromadb/chromadb.module';
 import { ExtractorAgent } from './extractor/extractor.agent';
-import { GoogleGeminiEmbeddingFunction } from '@chroma-core/google-gemini';
+import { GoogleGeminiEmbeddingFunction } from "@chroma-core/google-gemini";
 import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
 import { Chroma } from '@langchain/community/vectorstores/chroma';
 import { EvaluationModule } from '../evaluation/evaluation.module';
 
 @Module({
-    imports: [
-        MongodbModule,
-        ChromaDbModule,
-        forwardRef(() => EvaluationModule),
-    ],
-    providers: [
-        RagAgent,
-        ExtractorAgent,
-        {
-            provide: GoogleGeminiEmbeddingFunction,
-            useFactory: () => {
-                const apiKey = process.env.GEMINI_API_KEY;
-                if (!apiKey) throw new Error('Gemini API Key is not set');
+  imports: [
+    MongodbModule,
+    ChromaDbModule,
+    forwardRef(() => EvaluationModule)
+  ],
+  providers: [
+    RagAgent,
+    ExtractorAgent,
+    {
+      provide: GoogleGeminiEmbeddingFunction,
+      useFactory: () => {
+        const apiKey = process.env.GEMINI_API_KEY;
+        if (!apiKey) throw new Error("Gemini API Key is not set");
 
-                return new GoogleGeminiEmbeddingFunction({
-                    apiKey,
-                });
-            },
-        },
-        {
-            provide: GoogleGenerativeAIEmbeddings,
-            useFactory: () => {
-                const apiKey = process.env.GEMINI_API_KEY;
-                if (!apiKey) throw new Error('Gemini API Key is not set');
+        return new GoogleGeminiEmbeddingFunction({
+          apiKey
+        })
+      }
+    },
+    {
+      provide: GoogleGenerativeAIEmbeddings,
+      useFactory: () => {
+        const apiKey = process.env.GEMINI_API_KEY;
+        if (!apiKey) throw new Error("Gemini API Key is not set");
 
-                return new GoogleGenerativeAIEmbeddings({
-                    apiKey,
-                });
-            },
-        },
-        {
-            provide: Chroma,
-            inject: [GoogleGenerativeAIEmbeddings],
-            useFactory: (embedding: GoogleGenerativeAIEmbeddings) => {
-                const host = process.env.CHROMADB_HOST;
-                const port = process.env.CHROMADB_PORT;
-                const collectionName =
-                    process.env.CHROMA_RAG_COLLECTION_NAME ?? 'rag_collection';
+        return new GoogleGenerativeAIEmbeddings({
+          apiKey
+        });
+      }
+    },
+    {
+      provide: Chroma,
+      inject: [GoogleGenerativeAIEmbeddings],
+      useFactory: (embedding: GoogleGenerativeAIEmbeddings) => {
+        const host = process.env.CHROMADB_HOST;
+        const port = process.env.CHROMADB_PORT;
+        const collectionName = process.env.CHROMA_RAG_COLLECTION_NAME ?? 'rag_collection';
 
-                let url = process.env.CHROMA_URI;
-                if (url)
-                    return new Chroma(embedding, {
-                        collectionName,
-                        url,
-                    });
+        const apiKey = process.env.CHROMADB_API_KEY;
 
-                if (!host || !port) throw new Error('Chroma DB Env is not set');
+        if (apiKey)
+          return new Chroma(embedding, {
+            collectionName,
+          });
 
-                url = `http://${host}:${port}`;
-                return new Chroma(embedding, {
-                    collectionName,
-                    url,
-                });
-            },
-        },
-    ],
-    exports: [RagAgent, ExtractorAgent, GoogleGeminiEmbeddingFunction, Chroma],
+        if (!host || !port) throw new Error("Chroma DB Env is not set");
+
+        const url = `http://${host}:${port}`;
+        return new Chroma(embedding, {
+          collectionName,
+          url,
+        })
+      }
+    }
+  ],
+  exports: [RagAgent, ExtractorAgent, GoogleGeminiEmbeddingFunction]
 })
-export class AgentModule {}
+export class AgentModule { }
