604b6698353969cb59fd1da796624d021738c559
JC <jendycaprico@gmail.com>
2025-11-11 15:51:10 +0700

    fix/rag
    - fix(agent): fix chat history is messing up with result
    - fxi(tool): fix tool schema to let ai know the input for lists is optional

--- src/modules/agent/rag/rag-tool.schema.ts
+++ src/modules/agent/rag/rag-tool.schema.ts
@@ -6,6 +6,7 @@ export const GetCvResultsSchema = z.object({
         .describe(
             'Mongoose find query to retrieve CV results, by default it is limited to last 15. other than increase limit or manage sorting, use vector store to search more relevant information.',
         )
+        .optional()
         .default({ sort: { created_at: -1 }, limit: 15 }),
 });
 
@@ -13,7 +14,8 @@ export const GetListJobDescSchema = z.object({
     query: z
         .any()
         .describe(
-            'Mongoose find query to retrieve CV results, by default it is limited to last 15. other than increase limit or manage sorting, use vector store to search more relevant information.',
+            'Mongoose find query to retrieve job descriptions, by default it is limited to last 15. other than increase limit or manage sorting, use vector store to search more relevant information.',
         )
+        .optional()
         .default({ sort: { created_at: -1 }, limit: 15 }),
 });
