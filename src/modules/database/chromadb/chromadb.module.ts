8d38132075fa28cca8fd8ef36044500d3d30f0fb
JC <jendycaprico@gmail.com>
2025-11-11 18:52:57 +0700

    fix/chroma
    - fix(chroma): handle using chroma cloud

--- src/modules/database/chromadb/chromadb.module.ts
+++ src/modules/database/chromadb/chromadb.module.ts
@@ -1,27 +1,38 @@
 import { Module } from '@nestjs/common';
-import { ChromaClient } from 'chromadb';
+import { ChromaClient, CloudClient } from 'chromadb';
 
 @Module({
-    providers: [
-        {
-            provide: ChromaClient,
-            useFactory: () => {
-                return new ChromaClient({
-                    host: process.env.CHROMADB_HOST ?? 'localhost',
-                    port: +(process.env.CHROMADB_PORT ?? '8000'),
-                });
-            },
-        },
-    ],
-    exports: [ChromaClient],
+  providers: [
+    {
+      provide: ChromaClient,
+      useFactory: () => {
+        const apiKey = process.env.CHROMADB_API_KEY;
+        const tenant = process.env.CHROMADB_TENANT;
+        const database = process.env.CHROMADB_DB_NAME;
+
+        if (apiKey && tenant && database)
+          return new CloudClient({
+            apiKey,
+            tenant,
+            database
+          });
+
+        return new ChromaClient({
+          host: process.env.CHROMADB_HOST ?? 'localhost',
+          port: +(process.env.CHROMADB_PORT ?? '8000'),
+        });
+      },
+    },
+  ],
+  exports: [ChromaClient],
 })
 export class ChromaDbModule {
-    constructor(private readonly chromaDb: ChromaClient) {}
+  constructor(private readonly chromaDb: ChromaClient) { }
 
-    async onModuleInit() {
-        //   await Promise.all([
-        //     this.chromaDb.deleteCollection({ name: process.env.CHROMA_COLLECTION_NAME ?? "cv_collection" }),
-        //     this.chromaDb.deleteCollection({ name: process.env.CHROMA_RAG_COLLECTION_NAME ?? 'rag_collection' })
-        //   ]);
-    }
+  async onModuleInit() {
+    //   await Promise.all([
+    //     this.chromaDb.deleteCollection({ name: process.env.CHROMA_COLLECTION_NAME ?? "cv_collection" }),
+    //     this.chromaDb.deleteCollection({ name: process.env.CHROMA_RAG_COLLECTION_NAME ?? 'rag_collection' })
+    //   ]);
+  }
 }
